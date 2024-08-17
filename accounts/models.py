75cceadc9bda6cf866f94bdb374a51b4c6cc74dd
Shamim <shamimahmed82245@gmail.com>
2024-08-17 14:21:00 +0600

    one to one rel

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Husband(models.Model):
+    name = models.CharField(max_length=50)
+
+class Wife(models.Model):
+    name = models.CharField(max_length=50)
+    husband = models.OneToOneField(Husband,on_delete=models.CASCADE)
